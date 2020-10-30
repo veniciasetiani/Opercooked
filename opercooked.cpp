e3cd7ca57842ef6924dac725ae3a70fcd8149f71
IkanAsinn <73633566+IkanAsinn@users.noreply.github.com>
2020-10-30 23:05:27 +0700

    penambahan validasi input menu

--- opercooked.cpp
+++ opercooked.cpp
@@ -125,20 +125,24 @@ void order(){
             //kalau dia dessert
             for (int i = 1; i < 100; i++)
             {
-
-            if (strcmp(typeMenu[i], "Dessert") == 0)
+                if (strcmp(typeMenu[i], "Dessert") == 0)
+                {
+                    printf ("| %-5d| %-20s| %-7d| %-12s| %-11.2lf| %-11s| %-6s|\n", i, menuName[i], menuPrice[i], topping[i], calories[i], "-", "-");
+                }
+                //kalau dia drink
+                if (strcmp(typeMenu[i], "Drink") == 0)
+                {
+                    printf ("| %-5d| %-20s| %-7d| %-12s| %-11.2lf| %-11s| %-6s|\n", i, menuName[i], menuPrice[i], topping[i], calories[i], menuFlavor[i], menuSize[i]);
+                }
+            }
+            
+            while(1)
             {
-            printf ("| %-5d| %-20s| %-7d| %-12s| %-11.2lf| %-11s| %-6s|\n", i, menuName[i], menuPrice[i], topping[i], calories[i], "-", "-");
-            }
-            //kalau dia drink
-            if (strcmp(typeMenu[i], "Drink") == 0)
-            {
-            printf ("| %-5d| %-20s| %-7d| %-12s| %-11.2lf| %-11s| %-6s|\n", i, menuName[i], menuPrice[i], topping[i], calories[i], menuFlavor[i], menuSize[i]);
-            }
-            }
             printf ("Choose a menu to order [1 - %d]: ", currentMenu - 1);
             int input;
             scanf ("%d", &input); getchar();
+            if (input >= 1 && input <= currentMenu - 1) break;
+            }
             printf("\nSuccessfully add to order list!\n");
             printf ("Press Enter to continue"); getchar();
         }
